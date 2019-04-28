bbe156d25f25f04803f299be326f23af7c3b9733
Daniel <Dan.duda2@wp.pl>
2019-04-28 16:55:01 +0200

    Background color change

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ $(() => {
     const button = $('#button');
     button.click(() => {        
         clicks++;
-         counter.html(clicks)
+        counter.html(clicks)
     })
 
+    const inputs = $('input');
+    inputs.change((a) => {
+        $('.test').css(
+            'background-color',
+            `rgba(${inputs[0].value}, ${inputs[1].value}, ${inputs[2].value}, ${inputs[3].value})`,
 
+        )
+    })
 });
